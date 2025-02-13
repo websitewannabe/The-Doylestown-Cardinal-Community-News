a97e70310d85fd46fa1434af14c60a767617e066
websitewannabe <137509892+websitewannabe@users.noreply.github.com>
2025-02-13 19:35:14 +0000

    Assistant checkpoint: Update hero section divider styling
    Assistant generated file changes:
    - client/src/pages/HomePage.tsx: Update hero section divider line
    
    ---
    
    User prompt:
    
    On the home page hero, turn the color of the separation line the same color as the navigation bar. Also make the line longer

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -191,7 +191,7 @@ const HomePage = () => {
                 </Link>
               </div>
             </div>
-            <div className="w-1 h-48 bg-white/50 mx-8"></div>
+            <div className="w-1 h-64 bg-[#F2F0EF] mx-8"></div>
             <div className="w-60 h-60 pr-4 md:pr-8 lg:pr-12">
               <img
                 src="/images/Favicon_Cardinal-Icon_512.png"
